4c4ae258ace0332d11171ea32528f1dfef7b2177
Moiseis-Cruz <moizzito@gmail.com>
2023-04-19 21:13:04 -0300

    Comecei a programção do js.

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+const btnEnviar = document.getElementById("btn-enviar")
+
+let email = document.getElementById("email")
+
+btnEnviar.addEventListener("click", function () {
+    let formControl = document.getElementsByClassName("form-control")
+
+    console.log(formControl);
+})
