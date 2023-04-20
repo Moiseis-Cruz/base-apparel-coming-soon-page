228b8e737755b3421f40d1004b40f4e1b8d732fa
Moiseis-Cruz <moizzito@gmail.com>
2023-04-19 21:20:24 -0300

    Finalizando o js.

--- src/js/index.js
+++ src/js/index.js
@@ -2,8 +2,14 @@ const btnEnviar = document.getElementById("btn-enviar")
 
 let email = document.getElementById("email")
 
-btnEnviar.addEventListener("click", function () {
+btnEnviar.addEventListener("click", function (e) {
+    e.preventDefault()
+
     let formControl = document.getElementsByClassName("form-control")
 
-    console.log(formControl);
+    if(email.value.trim() === ""){
+        formControl[0].classList.add("invalid")
+    }else{
+        formControl[0].classList.remove("invalid")
+    }
 })
