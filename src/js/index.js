6a0ace8849d14d887ec24e21848006db0d546a2f
Moiseis-Cruz <moizzito@gmail.com>
2023-05-06 18:41:10 -0300

    Mudei o evento de click para submit.

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,35 @@
-const btnEnviar = document.getElementById("btn-enviar")
+// const btnEnviar = document.getElementById("btn-enviar")
 
-let email = document.getElementById("email")
+// let email = document.getElementById("email")
 
-btnEnviar.addEventListener("click", function (e) {
+// btnEnviar.addEventListener("click", function (e) {
+//     e.preventDefault()
+
+//     let formControl = document.getElementsByClassName("form-control")
+
+//     if(email.value.trim() === ""){
+//         formControl[0].classList.add("invalid")
+//     }else{
+//         formControl[0].classList.remove("invalid")
+//     }
+// })
+
+const form = document.getElementById("form")
+
+form.addEventListener("submit", function(e) {
     e.preventDefault()
 
-    let formControl = document.getElementsByClassName("form-control")
-
-    if(email.value.trim() === ""){
-        formControl[0].classList.add("invalid")
+    let formControl = document.querySelector(".form-control")
+    
+    let email = document.getElementById("email")
+    
+    let emailValue = email.value.trim()
+    
+    if(emailValue === ""){
+        formControl.classList.remove("enabled")
+        //formControl.classList.add("invalid")
     }else{
-        formControl[0].classList.remove("invalid")
+        formControl.classList.remove("invalid")
+        //formControl.classList.add("enabled")
     }
 })
