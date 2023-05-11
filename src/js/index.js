b6a66e1f9a879e99fa9c54d6da75f07cab887294
Moiseis-Cruz <moizzito@gmail.com>
2023-05-10 21:50:39 -0300

    Refiz o todo o código com função de "submit"

--- src/js/index.js
+++ src/js/index.js
@@ -14,22 +14,22 @@
 //     }
 // })
 
-const form = document.getElementById("form")
+const form = document.getElementById("form");
 
-form.addEventListener("submit", function(e) {
-    e.preventDefault()
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
 
-    let formControl = document.querySelector(".form-control")
-    
-    let email = document.getElementById("email")
-    
-    let emailValue = email.value.trim()
-    
-    if(emailValue === ""){
-        //formControl.classList.remove("enabled")
-        formControl.classList.add("invalid")
-    }else{
-        formControl.classList.remove("invalid")
-        //formControl.classList.add("enabled")
-    }
-})
+  let formControl = document.querySelector(".form-control");
+
+  let email = document.getElementById("email");
+
+  let emailValue = email.value.trim();
+
+  if (emailValue === "") {
+    // formControl.classList.remove("enabled")
+    formControl.classList.add("invalid");
+  } else {
+    formControl.classList.remove("invalid");
+    //formControl.classList.add("enabled")
+  }
+});
